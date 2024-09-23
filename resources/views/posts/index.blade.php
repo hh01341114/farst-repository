1f729d5b93aa43ee923d4c4d4d93b238104001b8
hh01341114 <shonan.hh1114@docomo.ne.jp>
2024-09-23 09:28:38 +0900

    ブログ投稿一覧表示

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <!-- headの内容 -->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blog</title>
+    <!-- Google Fontsの読み込み -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200..1000;1,200..1000&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <!--bodyの内容-->
+    <header>
+        <h1>Blog Name</h1>
+    </header>
+
+    <main>
+        <div class="posts">
+            @foreach ($posts as $post)
+                <div class="post">
+                    <h2 class="title">{{ $post->title }}</h2>
+                    <p class="body">{{ $post->body }}</p>
+                </div>
+            @endforeach        
+        </div>
+
+    </main>
+        
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\PostController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -17,7 +17,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-use App\Http\Controllers\PostController;
-
-
-Route::get('/posts', [PostController::class, 'index']);
+Route::get('/', [PostController::class, 'index']);
