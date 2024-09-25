c536f7a7a317048bc5602d45cf05bdfdafeab0ac
hh01341114 <shonan.hh1114@docomo.ne.jp>
2024-09-26 06:00:27 +0900

    詳細ページ

---
+++ resources/views/posts/show.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Posts</title>
+    <!--Googleフォント-->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200..1000;1,200..1000&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <header>
+        <h1 class="title">
+            {{ $post->title }}
+        </h1>
+    </header>
+
+    <main>
+        <div class="content">
+            <div class="content_post">
+                <h3>本文</h3>
+                <p>{{ $post->body }}</p>
+            </div>
+        </div>
+    </main>
+
+    <footer>
+        <div class="footer">
+            <a href="/">投稿一覧に戻る</a>
+        </div>
+    </footer>
+    
+</body>
+</html>
